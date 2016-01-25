55aa3c80febbd32b363f3037e85e1ef8e372599d
GraffC <christof.graff@comparex.de>
2016-01-25 09:09:11 +0100

    commit 2

--- source/ConsoleApplication1/ConsoleApplication1/ConsoleHelper.cs
+++ source/ConsoleApplication1/ConsoleApplication1/ConsoleHelper.cs
@@ -6,12 +6,7 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class ConsoleHelper
     {
-        static void Main(string[] args)
-        {
-            var helper = new ConsoleHelper();
-
-        }
     }
 }
